5b38503b729425645660c490087641bcaf0a7355
Cesar Gimenes <crg@crg.eti.br>
2016-06-04 13:00:01 -0300

    update

--- rotateString.go
+++ rotateString.go
@@ -1,5 +1,10 @@
 package rotateString
 
+/*
+Rotate recebe uma string e retorna a string rotacionada "invertida"
+
+TODO: Essa não tem um bom desempenho ela realoca muita memória, reescrever usando um buffer.
+*/
 func Rotate(s string) (result string) {
 	for _, v := range s {
 		result = string(v) + result
